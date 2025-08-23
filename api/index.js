a92b93d2e989976b4c5708a49cd748bcc55cbcb9
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-08-23 16:36:16 +0530

    chnage cors

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",  
+  origin: process.env.CORS_ORIGIN,
   credentials: true
 }));
 
